1c39aff509fe7aec11af4689091654edf048f734
Trinity <thutson3876@gmail.com>
2025-05-12 00:29:13 -0500

    All tests use same query data

--- CS210_Final_Project/OutputOption.cpp
+++ CS210_Final_Project/OutputOption.cpp
@@ -4,9 +4,14 @@
 
 using namespace std;
 
+static std::vector<std::tuple<std::string, std::string>> randomQueries;
+
 void OutputOption::action() {
 	auto menu = Menu::getInstance();
-	auto queries = menu->generateRandomQueries();
+
+	if(randomQueries.size() == 0)
+		randomQueries = menu->generateRandomQueries();
+
 	ICache* cache = menu->getCache();
 
 	string cacheType = menu->getCacheName();
@@ -14,7 +19,7 @@ void OutputOption::action() {
 	Timer timer;
 	
 
-	for (auto& q : queries) {
+	for (auto& q : randomQueries) {
 		string countryCode = get<0>(q);
 		string cityName = get<1>(q);
 
@@ -47,31 +52,4 @@ void OutputOption::action() {
 	cout << endl;
 
 	cout << "Performance test complete!" << endl;
-
-
-	/*string input;
-	auto menu = Menu::getInstance();
-	
-	cout << "Toggle Performance Tracking: 1. True | 2. False" << endl;
-
-	cin >> input;
-
-	if (!is_number(input)) {
-		cout << "Invalid Input: Must be a number" << endl;
-		return;
-	}
-
-	int i = stoi(input) - 1;
-
-	if (i < 0 || i >= 1) {
-		cout << "Invalid Input: Must be between 1 and 2" << endl;
-		return;
-	}
-
-	bool doTracking = i == 1 ? true : false;
-
-	menu->setTracking(doTracking);
-	
-	cout << "Performance Tracking set to " << doTracking << endl;
-	*/
 }
